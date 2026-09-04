f6a387eafab9c126b86bcefa4cf0632616f29db4
João Brumati <joao.brumati@amalfis.com.br>
2026-09-04 15:38:21 -0300

    Atualizacao do js

--- Principal/Javascript/script.js
+++ Principal/Javascript/script.js
@@ -1,27 +1,3 @@
-function dados(nome, idade, peso, altura) {
-    nome = prompt('Informe o seu nome');
-    idade = prompt('Informe a sua idade');
-    peso = Number(prompt('Informe o seu peso'));
-    altura = Number(prompt('Informe a sua altura'));
 
-    var imc = peso / (altura ** 2);
 
-    return imc;
-}
-
-var resultado = dados();
-
-console.log('Seu IMC é:', resultado);
-
-if (resultado < 18.5) {
-    console.log('Abaixo do peso');
-
-} else if (resultado >= 18.5 && resultado <= 24.9) {
-    console.log('Peso normal');
-
-} else if (resultado >= 25 && resultado <= 29.9) {
-    console.log('Sobrepeso');
-
-} else {
-    console.log('Obesidade');
-}
+const animais = document.getElementById('animais');
